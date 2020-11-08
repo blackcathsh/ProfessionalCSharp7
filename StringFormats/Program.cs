d0960b08c1238e1b5040d0ed80b17603341a79dd
huangshenghua <streetlighthsh@outlook.com>
2020-11-08 22:29:31 +0800

    StringsAndRegularExpressions

---
+++ StringFormats/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringFormats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person { FirstName = "Stephanie", LastName = "Nagel" };
+            Console.WriteLine(p1.ToString("F", null));
+            Console.WriteLine($"{p1:F}");
+        }
+    }
+}
