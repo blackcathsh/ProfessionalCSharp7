8f0feeed9351df7c767dab58bd927a4b9e416ef0
Shenghua Huang <streetlighthsh@outlook.com>
2020-11-15 21:10:36 +0800

    SpecialCollections

---
+++ PipelneSample/ColoredConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PipelneSample
+{
+    public class ColoredConsole
+    {
+        private static object _syncOutput = new object();
+
+        public static void WriteLine(string message)
+        {
+            lock (_syncOutput)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public static void WriteLine(string message, string color)
+        {
+            lock (_syncOutput)
+            {
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color);
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ PipelneSample/Info.cs
@@ -0,0 +1,20 @@
+﻿namespace PipelneSample
+{
+    public class Info
+    {
+        public Info(string word, int count)
+        {
+            Word = word;
+            Count = count;
+        }
+
+        public string Word { get; }
+        public int Count { get; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Count} times: {Word}";
+        }
+    }
+}
